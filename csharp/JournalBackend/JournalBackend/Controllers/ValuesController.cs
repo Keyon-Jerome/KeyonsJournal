29be063447857d06688555350e1dcf42223f72f6
Keyon Jerome <kjerome@stepsoftware.com>
2019-07-24 14:33:14 -0400

    Added MS SQL connection to C#.

--- csharp/JournalBackend/JournalBackend/Controllers/ValuesController.cs
+++ csharp/JournalBackend/JournalBackend/Controllers/ValuesController.cs
@@ -5,10 +5,13 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
+using System.Data.SqlClient;
 namespace JournalBackend.Controllers
 {
     public class ValuesController : ApiController
     {
+        string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\xampp\\htdocs\\KeyonsJournal\\csharp\\JournalBackend\\JournalBackend\\App_Data\\JournalDatabase.mdf;Integrated Security=True";
+        
         // GET api/values
         public IEnumerable<string> Get()
         {
@@ -24,6 +27,7 @@ namespace JournalBackend.Controllers
         // POST api/values
         public void Post([FromBody]string value)
         {
+            runQuery("INSERT INTO UserTable (Username,Password, Email) VALUES ('MaddestOfMad','362790964d0c8d305930575f60e2d3d048184d79','sample@example.com');");
         }
 
         // PUT api/values/5
@@ -34,6 +38,24 @@ namespace JournalBackend.Controllers
         // DELETE api/values/5
         public void Delete(int id)
         {
+        }
+        public void runQuery(string queryString)
+        {
+            using (SqlConnection connection = new SqlConnection(
+               connectionString))
+            {
+                SqlCommand command = new SqlCommand(
+                    queryString, connection);
+                connection.Open();
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(String.Format("{0}, {1}",
+                            reader[0], reader[1]));
+                    }
+                }
+            }
         }
     }
 }
