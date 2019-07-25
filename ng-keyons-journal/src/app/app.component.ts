76492ebc5d6495d66760cc22d65f521c40c5b0e8
Keyon Jerome <kjerome@stepsoftware.com>
2019-07-25 15:58:01 -0400

    Cordova fix attempts.

--- ng-keyons-journal/src/app/app.component.ts
+++ ng-keyons-journal/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+declare var device;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  {
   title = 'ng-keyons-journal';
+
+  ngOnInit() {
+    // document.addEventListener(“deviceready”, function() {
+    //   alert(device.platform);
+    //   }, false);
+  }
 }
